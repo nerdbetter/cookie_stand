ed0c12fccd6c0cb459bc01c8272bb49881e7524e
nerdbetter <jasontlogan@gmail.com>
2017-06-12 15:04:30 -0500

    random generate customer for cookie amount

--- modal.js
+++ modal.js
@@ -1,1 +1,10 @@
 ('use strict');
+
+var firstAndPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiePerSale: 6.3,
+  getCookieNeed: function neededCookies(){
+    return Math.random(this.minCustomers,this.maxCustomers) * this.avgCookiePerSale;
+  }
+};
