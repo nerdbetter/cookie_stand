3d60c406825e0f17392b8f130fe8044b67fed14b
nerdbetter <jasontlogan@gmail.com>
2017-06-13 15:28:25 -0500

    added correct variables

--- model.js
+++ model.js
@@ -1,19 +1,15 @@
 ('use strict');
 
-function sum(a,b){
-  var calculatedSum = a + b;
-  return calculatedSum;
-}
-
 function randomCustomer(min,max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function CookieStore(location, cookiePerHour, hoursOfOperation, minCustomers, maxCustomers, avgCookiePerSale ){
+function CookieStore(location, minCustomers, maxCustomers, avgCookiePerSale ){
   this.location = location;
-  this.cookiePerHour = [];
+  this.cookiePerHour = [];// can't get this to pass properly in totalCookieSum()
   this.hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ];
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
+  this.avgCookiePerSale = avgCookiePerSale;
 }
 CookieStore.prototype.editDoc = function (){
   var newEl = document.createElement('h3');
@@ -23,24 +19,21 @@ CookieStore.prototype.editDoc = function (){
 CookieStore.prototype.getCookieNeed = function (){
   for (var i = 0; i < this.hoursOfOperation.length; i++){
     var cookies = randomCustomer( this.minCustomers , this.maxCustomers ) * this.avgCookiePerSale;
-    console.log (cookies);
+    //console.log (cookies);
     this.cookiePerHour[i] = cookies;
-    console.log (this.cookiePerHour);
+    //console.log (this.cookiePerHour);
   }
 };
 CookieStore.prototype.totalCookieSum = function (){
-  var calculatedSum = 0;
-  for(var i = 0; i < this.cookiePerHour.length; i++) {
-    var valueFromArray = this.cookiePerHour[i];
-    calculatedSum = sum(valueFromArray, calculatedSum)[0];
-  }
+  var calculatedSum = 0; var i = this.cookiePerHour.length; while(i--) calculatedSum += this.cookiePerHour[i];
+  console.log(calculatedSum);
 };
 
-var location1 = new CookieStore('First and Pike', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ], 23, 65, 6.3);
-var location2 = new CookieStore('SeaTac Airport', 15, 20, 75);
-var location3 = new CookieStore('Tres');
-var location4 = new CookieStore('Tres');
-var location5 = new CookieStore('Tres');
+var location1 = new CookieStore('First and Pike', 23, 65, 6.3);
+var location2 = new CookieStore('SeaTac Airport', 3, 24, 1.2);
+var location3 = new CookieStore('Seattle Center', 11, 38, 3.7);
+var location4 = new CookieStore('Capitol Hill', 20, 38, 2.3);
+var location5 = new CookieStore('Alki', 2, 16, 4.6);
 
 var stores = [location1, location2, location3, location4, location5];
 location1.getCookieNeed();
@@ -63,4 +56,4 @@ location5.getCookieNeed();
 location5.totalCookieSum();
 location5.editDoc();
 
-console.log(myAwesomeStores);
+console.log(stores);
