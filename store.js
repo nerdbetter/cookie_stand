04e4b42e4910860d25decf2a8ad7be22f6e3d1c1
nerdbetter <jasontlogan@gmail.com>
2017-06-15 16:13:57 -0500

    everything works!

--- store.js
+++ store.js
@@ -1,35 +1,40 @@
 // functions to get time to add
 CookieStore.prototype.timeBar = function (){
-  var storeTimeContainer = document.getElementById('storeTimes');
   var timeRow = document.createElement('tr');
-  for (var k = 0; k < hours.length; k++) {
-    var time = hours[k];
-    console.log(time);
-    addTimeRow(storeTimeContainer, time);
-  }
-  function addTimeRow(storeTimeContainer, time) {
-    var storeTimeCell = document.createElement('td');
-    storeTimeCell.textContent = time;
-    storeTimeContainer.appendChild(timeRow);
-    timeRow.appendChild(storeTimeCell);
+  var storeTimeContainer = document.getElementById('storeTimes');
+  storeTimeContainer.appendChild(timeRow);
+  var blankCell = document.createElement('td');
+  timeRow.appendChild(blankCell);
+  timeRow.appendChild(blankCell);
+  for (var k = 0; k < this.hoursOfOperation.length; k++) {
+    var time = this.hoursOfOperation[k];
+    var newTime =  document.createElement('td');
+    var newTimeText = document.createTextNode(time);
+    newTime.appendChild(newTimeText);
+    timeRow.appendChild(newTime);
   }
 };
 location1.timeBar();
+
 CookieStore.prototype.render = function (){
   var newAddress = document.createElement('td');
   var addressText = document.createTextNode(this.address);
   newAddress.appendChild(addressText);
-  var addressContainer = document.getElementsByTagName('tbody')[0];
-  addressContainer.setAttribute('colspan', 2);
-  addressContainer.appendChild(newAddress);
+  var newRow = document.createElement('tr');
+  newRow.appendChild(newAddress);
+  var tableContainer = document.getElementsByTagName('tbody')[0];
+  tableContainer.appendChild(newRow);
 
   for ( i = 0; i < this.cookiePerHour.length; i++ ){
     var cookies = this.cookiePerHour[i];
     var newCookiePerHour = document.createElement('td');
     var cookiePerHourText = document.createTextNode(cookies);
     newCookiePerHour.appendChild(cookiePerHourText);
-    var cookieJar = document.getElementsByTagName('tbody')[0];
-    cookieJar.appendChild(newCookiePerHour);
+    newRow.appendChild(newCookiePerHour);
   }
 };
 location1.render();
+location2.render();
+location3.render();
+location4.render();
+location5.render();
