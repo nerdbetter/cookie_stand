773922a6ca6a00cd68a974375a62dd0f8ff95584
nerdbetter <jasontlogan@gmail.com>
2017-06-16 10:42:46 -0500

    working slideshow

--- index.js
+++ index.js
@@ -17,25 +17,3 @@ function formSubmit(event){
   console.log (newLocation);
   newLocation.render();
 };
-//slides function
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('slides');
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-  }
-  slides[slideIndex - 1].style.display = 'block';
-}
